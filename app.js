72245b10099fe190a8c0e7ebc4f83032450ba126
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-24 21:49:33 +0100

    Security

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
+
+// extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 const express = require("express");
 // DB CONNECTION
 const databaseConnection = require("./db/connect");
@@ -16,6 +23,16 @@ const routerJobs = require("./routes/jobs");
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.search("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 60, //
+    max: 100, //max request (100 reequest per windowMs)
+  })
+);
 // extra packages
 
 // routes
