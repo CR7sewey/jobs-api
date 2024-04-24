f49e421bd85379d22766523017f2c089770d1e7a
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-24 18:41:36 +0100

    UpdateJob controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,6 +1,7 @@
 const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 const NotFound = require("../errors/not-found-error");
+const BadRequestError = require("../errors/bad-request-error");
 
 const getAllJobs = async (req, res) => {
   const all_jobs = await Job.find({ createdBy: req.user._id }).sort({
@@ -31,7 +32,23 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-  return res.send("Get Job 3");
+  const createdBy = req.user._id;
+  const { id } = req.params;
+  if (
+    typeof req.body.company === "undefined" ||
+    typeof req.body.position === "undefined"
+  ) {
+    throw new BadRequestError("Company or Position fields cannot be empty");
+  }
+
+  const job = await Job.findByIdAndUpdate({ createdBy, _id: id }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!job) {
+    throw new NotFound("You dont have a job registered with this id");
+  }
+  return res.status(200).json({ job });
 };
 
 const deleteJob = async (req, res) => {
