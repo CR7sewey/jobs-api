9dfa45563dafd60f4330144b6ced5c4e05f2f955
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-24 18:11:19 +0100

    CreateJob and GetAllJobs controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,5 +1,9 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+
 const getAllJobs = async (req, res) => {
-  return res.send("Get Job");
+  const all_jobs = await Job.find({});
+  return res.status(200).json({ all_jobs });
 };
 
 const getJob = async (req, res) => {
@@ -7,7 +11,14 @@ const getJob = async (req, res) => {
 };
 
 const createJob = async (req, res) => {
-  return res.json(req.user);
+  console.log(req.user);
+  const { _id: id } = req.user;
+  if (!id) {
+    throw new Error("Well not supposed to be here!");
+  }
+  const new_job = await Job.create({ ...req.body, createdBy: id });
+
+  return res.status(StatusCodes.CREATED).json({ new_job });
 };
 
 const updateJob = async (req, res) => {
